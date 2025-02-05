2ccb651f83a302050c40ca8129cc39fe11b0bbf7
Emerson <emerson.cesario@elven.works>
2025-02-05 10:49:34 -0300

    feat: add test local

--- local/otel.sh
+++ local/otel.sh
@@ -8,3 +8,5 @@ go install github.com/open-telemetry/opentelemetry-collector-contrib/cmd/telemet
 sudo cp bin/telemetrygen /usr/local/bin/
 
 docker run -p 4317:4317 -p 8888:8888 -v $(pwd)/config.yaml:/etc/otelcol-contrib/config.yaml ghcr.io/open-telemetry/opentelemetry-collector-releases/opentelemetry-collector-contrib:0.86.0
+
+## telemetrygen traces --otlp-insecure --traces 1
